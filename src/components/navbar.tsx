a941b4015e29c2975d22897e5e55bd22ad2b3b47
meuzi8122 <kagiakete1234@gmail.com>
2025-09-24 16:12:49 +0900

    navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,7 @@
+export function Navbar() {
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <a className="btn btn-ghost text-xl">TablatureDB</a>
+        </div>
+    );
+}
