c9cb626dac5ff49a490f2dc11de0afe27f3924f4
meuzi8122 <kagiakete1234@gmail.com>
2025-09-24 17:31:50 +0900

    fix navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export function Navbar() {
     return (
         <div className="navbar bg-base-100 shadow-sm mb-6">
-            <a className="btn btn-ghost text-xl">TablatureDB</a>
+            <Link href="/" className="btn btn-ghost text-xl">
+                TablatureDB
+            </Link>
         </div>
     );
 }
