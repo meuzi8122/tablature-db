f63163c2d5fdef782741c059c5d780ce28666fc2
meuzi8122 <kagiakete1234@gmail.com>
2025-10-13 15:43:40 +0900

    fix navbarr

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export function Navbar() {
     return (
-        <div className="navbar bg-base-100 shadow-sm mb-6">
+        <div className="navbar bg-primary shadow-sm mb-6">
             <div className="flex-1">
-                <Link href="/" className="btn btn-ghost text-xl">
+                <Link href="/" className="btn btn-ghost text-xl text-white font-bold">
                     TablatureDB
                 </Link>
             </div>
             <div className="flex-none">
-                <Link href="/tablatures/new" className="btn btn-circle btn-ghost">
+                <Link href="/tablatures/new" className="btn btn-circle btn-primary">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
