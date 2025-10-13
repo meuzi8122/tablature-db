15657a236d7ab338a037c1a4415918e9b71cddb0
meuzi8122 <kagiakete1234@gmail.com>
2025-10-13 16:18:07 +0900

    delete h1

--- src/app/tablatures/new/page.tsx
+++ src/app/tablatures/new/page.tsx
@@ -3,7 +3,6 @@ import { TablatureForm } from "@/components/tablature/tablature-form";
 export default async function TablatureNewPage() {
     return (
         <div className="container mx-auto mb-2 px-5 md:px-0">
-            <h1 className="mb-5 text-xl">TAB譜を投稿</h1>
             <TablatureForm />
         </div>
     );
