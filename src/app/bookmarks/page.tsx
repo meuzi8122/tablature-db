cef6a694dc0fbe080f52b0ecb863e7fddcb85602
meuzi8122 <kagiakete1234@gmail.com>
2025-10-21 12:38:29 +0900

    fix import

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,5 +1,4 @@
 import { ClientBookmarkedTablatureList } from "@/components/tablature/tablature-list/tablature-list.bookmark.client";
-import { TablatureSearch } from "@/components/tablature/tablature-search";
 
 export default async function BookmarkPage() {
     return (
